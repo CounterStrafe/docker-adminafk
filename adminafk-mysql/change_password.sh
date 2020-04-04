30358fbae1d86164fa2550ebca1ab2b0011bc37c
Eric Oswald <eoswald@pm.me>
2020-04-04 02:27:50 -0400

    Also pass in AdminAFK MySQL password to change_password.sh

--- adminafk-mysql/change_password.sh
+++ adminafk-mysql/change_password.sh
@@ -1,9 +1,10 @@
 #! bin/bash
 
-ADMIN_PASSWORD_HASH="$1"
+ADMINAFK_PASSWORD_ADMINAFK="$1"
+ADMIN_PASSWORD_HASH="$2"
 
 # wait for database to come up
 while ! nc -z 127.0.0.1 3306; do sleep 3; done
 
-mysql -u adminafk --password="adminafk" -D adminafk -e "UPDATE \`users\` SET \`password\` = \"$ADMIN_PASSWORD_HASH\" WHERE \`login\` = \"admin\";"
+mysql -u adminafk --password="$ADMINAFK_PASSWORD_ADMINAFK" -D adminafk -e "UPDATE \`users\` SET \`password\` = \"$ADMIN_PASSWORD_HASH\" WHERE \`login\` = \"admin\";"
 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+ADMINAFK_PASSWORD_ADMINAFK=''
 ADMIN_PASSWORD_HASH=''
 
 docker-compose up -d
 
-docker exec adminafk-mysql /bin/change_password.sh $ADMIN_PASSWORD_HASH
+docker exec adminafk-mysql /bin/change_password.sh $ADMINAFK_PASSWORD_ADMINAFK $ADMIN_PASSWORD_HASH
 
