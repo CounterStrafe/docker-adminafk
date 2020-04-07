752a02f300d35ba9b7e81f6a0a8e65cba28da7ce
Eric Oswald <eoswald@pm.me>
2020-04-07 19:00:47 -0400

    Add TLS via Let's Encrypt and replace haproxy with nginx to server the static Let's Encrypt content.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
+curl -L https://raw.githubusercontent.com/wmnnd/nginx-certbot/cb13104649e80f31797d8b76ad374bcdee4874d9/init-letsencrypt.sh > init-letsencrypt.sh
+
+sed -i "s|example\.org www\.example\.org|test.com test.com|" init-letsencrypt.sh
+sed -i "s|email=\"\"|email=\"test@test.com\"|" init-letsencrypt.sh
+
 ADMINAFK_PASSWORD_ADMINAFK=''
 ADMIN_PASSWORD_HASH=''
 
-docker-compose up -d
+#docker-compose up -d
 
-docker exec adminafk-mysql /bin/change_password.sh $ADMINAFK_PASSWORD_ADMINAFK $ADMIN_PASSWORD_HASH
+#docker exec adminafk-mysql /bin/change_password.sh $ADMINAFK_PASSWORD_ADMINAFK $ADMIN_PASSWORD_HASH
 
